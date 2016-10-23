1f88c71712f7035d19218ce4cb414e7993d9081b
shulun <chenshulun@gmail.com>
2016-10-22 21:48:12 -0400

    36th

--- js/index.js
+++ js/index.js
@@ -15,6 +15,12 @@ $(document).ready(function() {
         $('#pagepiling').pagepiling({
             menu: '#menu',
             anchors: ['page1', 'page2', 'page3', 'page4', 'page5'],
+            navigation: {
+                    'textColor': '#f2f2f2',
+                    'bulletsColor': '#ccc',
+                    'position': 'right',
+                    'tooltips': ['Page 1', 'Page 2', 'Page 3', 'Page 4', 'Page 5']
+                },
             loopBottom: true,
             navigation: {
                 'position': 'left',
